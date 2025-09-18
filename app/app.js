60d1632c3f11683fea5ed2ca6dd94e766501797b
pbojar <preston.bowesjaros@gmail.com>
2025-09-18 18:24:57 -0400

    Refactor app.js

--- app/app.js
+++ app/app.js
@@ -1,12 +1,78 @@
-var letterBoard = document.getElementById("letter-board");
-var letters = document.getElementsByClassName("letter");
-var baseAngles = new Array(letters.length).fill(0);
+// BoggleBoard
+class BoggleBoard {
 
-const letterBoardWrap = document.getElementById("letter-board-wrap");
-const newGameButton = document.getElementById("new-game-button");
-const rotateButton = document.getElementById("rotate-button");
-const randomizeButton = document.getElementById("scramble-button");
-const alignButton = document.getElementById("align-button");
+    constructor() {
+        this.gameLetters = "";
+        this.boardAngle = 0;
+        this.letterAngles = new Array(this.gameLetters.length).fill(0);
+    }
+
+    newGame() {
+        let size = document.querySelector('input[name="board-size"]:checked').value;
+        this.gameLetters = genRandStr(size * size);
+
+        // Make html for newLetterBoard
+        let newLetterBoard = `<div class="letter-board board-${size}x${size}" id="letter-board">\n`;
+        for (let i = 0; i < this.gameLetters.length; i++) {
+            if (this.gameLetters[i] === 'q') {
+                newLetterBoard += `<div class="letter" data-idx="${i}"><span>qu</span></div>\n`;
+            } else {
+                newLetterBoard += `<div class="letter" data-idx="${i}"><span>${this.gameLetters[i]}</span></div>\n`;
+            }
+        }
+        newLetterBoard += '</div>';
+
+        // Get letterBoardWrap and insert newLetterBoard
+        const letterBoardWrap = document.getElementById("letter-board-wrap");
+        letterBoardWrap.innerHTML = newLetterBoard;
+        this.boardAngle = 0;
+        this.letterAngles = new Array(this.gameLetters.length).fill(0);
+    }
+
+    rotateBoard() {
+        // Get letterBoard
+        const letterBoard = document.getElementById("letter-board");
+
+        // Animate letterBoard
+        if (letterBoard) {
+            letterBoard.animate([
+                {transform: `scale(1) rotate(${this.boardAngle}deg)`},
+                {transform: `scale(0.65) rotate(${this.boardAngle+45}deg)`},
+                {transform: `scale(1) rotate(${this.boardAngle+90}deg)`}
+            ], {
+                duration: 1000,
+                fill: 'forwards',
+                easing: 'ease-in-out'
+            });
+
+            // Update boardAngle in class and rotation of letter elements in document
+            this.boardAngle += 90;
+            const letters = Array.from(document.getElementsByClassName("letter"));
+            for (let i = 0; i < letters.length; i++) {
+                let newAngle = this.letterAngles[i] - this.boardAngle;
+                letters[i].style.transform = `rotate(${newAngle}deg)`;
+            }
+        }
+    }
+
+    scrambleLetterOrientations() {
+        const letters = Array.from(document.getElementsByClassName("letter"));
+        this.letterAngles = getRandomAngles(letters.length, [0, 90, 180, 270])
+        for (let i = 0; i < letters.length; i++) {
+            let newAngle = this.letterAngles[i] - this.boardAngle;
+            letters[i].style.transform = `rotate(${newAngle}deg)`;
+        }
+    }
+
+    alignLetterOrientations() {
+        const letters = Array.from(document.getElementsByClassName("letter"));
+        this.letterAngles.fill(0)
+        for (let i = 0; i < letters.length; i++) {
+            let newAngle = this.letterAngles[i] - this.boardAngle;
+            letters[i].style.transform = `rotate(${newAngle}deg)`;
+        }
+    }
+}
 
 function getRandomAngle(angles) {
     return angles[Math.floor(Math.random() * angles.length)]
@@ -20,48 +86,6 @@ function getRandomAngles(n, angleSet) {
     return randomAngles
 }
 
-const options = {
-    duration: 1000,
-    fill: 'forwards',
-    easing: 'ease-in-out'
-};
-let angle = 0;
-function handleRotate() {
-    let keyframes = [
-        {transform: `scale(1) rotate(${angle}deg)`},
-        {transform: `scale(0.65) rotate(${angle+45}deg)`},
-        {transform: `scale(1) rotate(${angle+90}deg)`}
-    ]; 
-    letterBoard.animate(keyframes, options);
-    angle += 90;
-    for (let i = 0; i < letters.length; i++) {
-        newAngle = baseAngles[i] - angle;
-        letters[i].style.transform = `rotate(${newAngle}deg)`;
-    }
-}
-
-rotateButton.addEventListener("click", handleRotate)
-
-function handleRandomize() {
-    baseAngles = getRandomAngles(letters.length, [0, 90, 180, 270])
-    for (let i = 0; i < letters.length; i++) {
-        let newAngle = baseAngles[i] - angle
-        letters[i].style.transform = `rotate(${newAngle}deg)`;
-    }
-}
-
-randomizeButton.addEventListener("click", handleRandomize)
-
-function handleAlign() {
-    baseAngles.fill(0)
-    for (let i = 0; i < letters.length; i++) {
-        let newAngle = baseAngles[i] - angle
-        letters[i].style.transform = `rotate(${newAngle}deg)`;
-    }
-}
-
-alignButton.addEventListener("click", handleAlign)
-
 function genRandStr(len) {
     const chars = 'abcdefghijklmnopqrstuvwxyz'
     let str = '';
@@ -71,23 +95,19 @@ function genRandStr(len) {
     return str
 }
 
-function handleNewGame() {
-    let sz = document.querySelector('input[name="board-size"]:checked').value;
-    let newChars = genRandStr(sz * sz);
-    let newLetterBoard = `<div class="letter-board board-${sz}x${sz}" id="letter-board">\n`;
-    for (let i = 0; i < newChars.length; i++) {
-        if (newChars[i] === 'q') {
-            newLetterBoard += `<div class="letter" data-idx="${i}"><span>qu</span></div>\n`;
-        } else {
-            newLetterBoard += `<div class="letter" data-idx="${i}"><span>${newChars[i]}</span></div>\n`;
-        }
-    }
-    newLetterBoard += '</div>';
-    letterBoardWrap.innerHTML = newLetterBoard;
-    letterBoard = document.getElementById("letter-board");
-    letters = document.getElementsByClassName("letter");
-    baseAngles = new Array(letters.length).fill(0);
-    angle = 0;
-}
 
-newGameButton.addEventListener("click", handleNewGame)
+var board = new BoggleBoard();
+
+const newGameButton = document.getElementById("new-game-button");
+const rotateButton = document.getElementById("rotate-button");
+const scrambleButton = document.getElementById("scramble-button");
+const alignButton = document.getElementById("align-button");
+
+newGameButton.addEventListener("click", board.newGame);
+rotateButton.addEventListener("click", board.rotateBoard);
+scrambleButton.addEventListener("click", board.scrambleLetterOrientations);
+alignButton.addEventListener("click", board.alignLetterOrientations);
+
+window.onload = function() {
+    board.newGame();
+}
